c93d32f679b28c5ed77323bbb47a002389bb346e
Nick Bennett <nick@happyforyoursuccess.com>
2017-12-19 11:21:34 -0600

    Update GetMailboxSizeDetails.ps1

--- OnPrem/GetMailboxSizeDetails.ps1
+++ OnPrem/GetMailboxSizeDetails.ps1
@@ -1,2 +1,3 @@
+# Reference: https://technet.microsoft.com/en-us/library/aa996762(v=exchg.160).aspx
 Param($ALIAS)
 Get-MailboxFolderStatistics -Identity $ALIAS | Select-Object FolderPath, ItemsInFolder, FolderSize, FolderType | Sort-Object -Property ItemsInFolder -Descending
